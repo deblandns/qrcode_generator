2c191fab2ce9a405fb49f205f00104e1cff6e1dc
debland <hosseinnysyan1385@gmail.com>
2023-09-12 11:33:13 +0330

    initial configs and navbar style

--- qrcode_generator/settings.py
+++ qrcode_generator/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'qrcode_maker.apps.QrcodeMakerConfig',
+    'django_render_partial',
+
 
 ]
 
@@ -118,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- qrcode_maker/views.py
+++ qrcode_maker/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class homepage(TemplateView):
+    template_name = "homepage.html"
+
+
+def navbar_components(request):
+    return render(request, "shared/navbar.html")
